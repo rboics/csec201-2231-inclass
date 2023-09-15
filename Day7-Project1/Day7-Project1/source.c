f473a9d14aa6ee409827a0c7ca1bc39664195157
nerdprof <robert.olson.2018.googfi@gmail.com>
2023-09-15 18:04:10 -0400

    Pushing day 7, possible day 6

---
+++ Day7-Project1/Day7-Project1/source.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int fact(int n) {
+	int product = 1;
+	printf("n: %p, product: %p\n", &n, &product);
+	for (int i = 1; i <= n; i++) product = product * i;
+	return product;
+}
+
+//Pass by Value --> New copy of the variable
+int recFact(int n) {
+	int product = 1;
+	printf("n: %p, product: %p\n", &n, &product);
+	if (n == 1) return 1;
+	else {
+		product = n * recFact(n - 1);
+		return product;
+	}
+}
+
+//Pass-by-Reference
+int pbrRecFact(int* n) {
+
+
+}
+
+
+int main(void) {
+	int x;
+	int res;
+	printf("x: %p, res: %p\n", &x, &res);
+	printf("Enter a number: ");
+	scanf("%d", &x);
+	res = recFact(x);
+	printf("Factorial: %d", res);
+}
